bf483f7ee5f331d7eee04353a99306ae9ba5a7f8
axguowen <axguowen@qq.com>
2024-04-28 15:57:21 +0800

    修复变量值为空时模板文件不存在的错误

--- src/Template.php
+++ src/Template.php
@@ -1192,13 +1192,13 @@ class Template
         $parseStr = '';
 
         foreach ($array as $templateName) {
-            if (empty($templateName)) {
-                continue;
-            }
-
             if (str_starts_with($templateName, '$')) {
                 //支持加载变量文件名
                 $templateName = $this->get(substr($templateName, 1));
+            }
+
+            if (empty($templateName)) {
+                continue;
             }
 
             $template = $this->parseTemplateFile($templateName);
